2aedb0897dbc24945b860ffca9416407836dac84
69Degenerate <94638388+69Degenerate@users.noreply.github.com>
2023-01-15 20:13:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+import pymongo
 
 app = FastAPI()
+cli=pymongo.MongoClient("mongodb://mongo:hKkb23G8NtWIezPQe4Cg@containers-us-west-197.railway.app:7613")
+db=cli.workdb.emp
 
 class Msg(BaseModel):
     msg: str
@@ -25,3 +28,56 @@ async def demo_post(inp: Msg):
 @app.get("/path/{path_id}")
 async def demo_get_path_id(path_id: int):
     return {"message": f"This is /path/{path_id} endpoint, use post request to retrieve result"}
+
+
+
+@app.get("/read")
+def read():
+    s=db.find({"deptno": 30},{'_id':False})
+    print(s)
+    di = loads(dumps(list(s)))
+    return di
+
+
+@app.get("/readone/{deptno}")
+async def readone(deptno):
+    n=int(deptno)
+    return loads(dumps(list(db.find({"deptno": n},{'_id':False}))))
+
+
+@app.get('/readname/{ename}')
+def readname(ename):
+    return loads(dumps(list(db.find({"ename":{'$regex': ename,'$options': 'i'}},{'_id':False}))))
+
+
+@app.post("/items/")
+def insert():
+    data={
+    "empno": 7499,
+    "ename": "allen",
+    "job": "salesman",
+    "mgr": 7698,
+    "hiredate": "20-02-81",
+    "sal": 1600,
+    "comm": 300,
+    "deptno": 30
+  }
+    db.insert_one(data)
+    return dict(data)
+
+@app.delete('/delete/{name}')
+def delete(name):
+    db.delete_many({"ename":name})
+    s=db.find({"deptno": name},{'_id':False})
+    return loads(dumps(list(s)))
+
+
+@app.put('/update/')
+def update(name,newname):
+    myquery = { "ename": { "$regex": name } }
+    newvalues = { "$set": { "ename": newname } }
+    x = db.update_many(myquery, newvalues)
+    print(x.modified_count, "documents updated.")
+    s=db.find({"ename": newname},{'_id':False})
+    print(s)
+    return loads(dumps(list(s)))
